409777fe56cfa87af8384b1b0dbc19dbe7e79862
KEINOS <github@keinos.com>
2019-02-16 18:46:09 +0900

    feat: v0.0.1-20180216-1821

---
+++ Formula/testcmd.rb
@@ -0,0 +1,18 @@
+REPOSITORY_URL="https://github.com/KEINOS/testcmd".freeze
+HOMEBREW_TESTCMD_VERSION="0.0.1-20180216-1821".freeze
+
+class TestCmd < Formula
+  desc "Just echoes the hello world."
+  homepage REPOSITORY_URL
+  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_TESTCMD_VERSION}.tar.gz"
+  sha256 "28cf7f40e90350d59f9b5a852b9e868a5c909af42e9fbfda4c507620d31db7fc"
+  head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_TESTCMD_VERSION
+
+  def install
+    bin.install "bin/testcmd"
+  end
+
+  test do
+    system "false"
+  end
+end
