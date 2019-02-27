d689603ae4fb276dfd81f9d4b3ae2b358bd24184
KEINOS <github@keinos.com>
2019-02-27 14:19:10 +0900

    docs: Add help. v0.0.1-20180227-1417

--- Formula/testcmd.rb
+++ Formula/testcmd.rb
@@ -2,8 +2,8 @@ class Testcmd < Formula
   desc "Just echoes the hello world."
   homepage "https://github.com/KEINOS/homebrew-testcmd"
   url "https://KEINOS.github.io/homebrew-testcmd/archive.tar.gz"
-  sha256 "6326a07ec7704252332cf9200adb6861f6f10d3075f6c98a98c1a43da0b42a11"
-  version "v0.0.1-20180227-1316"
+  sha256 "a9710ab1c5e7281ad8665f263e9dd473ae20d119852ddaf6bd5f7914a042ef71"
+  version "v0.0.1-20180227-1417"
   head "https://github.com/KEINOS/homebrew-testcmd.git"
 
   def install
--- src/testcmd.sh
+++ src/testcmd.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 
-# salt: 1316
+ABOUT_COMMAND="This command just prints 'Hello World!' with a number but nothing more."
+NAME_COMMAND=`basename $0`
 
-echo 'Hello World!'
+# Change the salt below to change the hash value of the archive.
+SALT=1401
 
+# Define Help
+show_help() {
+    cat << EOL
+${NAME_COMMAND}
+    ${ABOUT_COMMAND}
+
+USAGE  : ${NAME_COMMAND} [-h]
+OPTION :
+    -h  This help
+EOL
+    exit $LINENO
+}
+
+# Option
+while getopts h OPT
+do
+    case $OPT in
+        h)  show_help
+            ;;
+        \?) usage_exit
+            ;;
+    esac
+done
+
+echo "Hello World! (${SALT})"
