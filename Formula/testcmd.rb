b93dea3dba473664bb33e73096d44b19710a091a
KEINOS <github@keinos.com>
2019-02-27 12:49:56 +0900

    v0.0.1-20180227-1246

--- Formula/testcmd.rb
+++ Formula/testcmd.rb
@@ -2,8 +2,8 @@ class Testcmd < Formula
   desc "Just echoes the hello world."
   homepage "https://github.com/KEINOS/homebrew-testcmd"
   url "https://KEINOS.github.io/homebrew-testcmd/archive.tar.gz"
-  sha256 "399f92c7756e4923411e0b3e3d2cca60daa773993e5e8aad034df4d0186b836b"
-  version "v0.0.1-20180227-1230"
+  sha256 "9acba6cc9b34b88975f62c67d460b85007098fac4efc2083ed4302e7497f88a9"
+  version "v0.0.1-20180227-1246"
   head "https://github.com/KEINOS/homebrew-testcmd.git"
 
   def install
